29fe11ea5514691f3c793fa749306fd71c1ff9e7
huntr <99586769+huntriscool@users.noreply.github.com>
2026-08-11 15:40:14 +1000

    update small files

--- practice_package/src/App.java
+++ practice_package/src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TEST
+        System.out.println(isEven(3)); // EXPECT FALSE
+        System.out.println(isOdd(3)); // EXPECT TRUE
+        System.out.println(sumOf(3,5)); // EXPECT 8
+    }
+
+    //sum 2 numbers
+    public static int sumOf(int a, int b) {
+        return a + b;
+    }
+
+    //is number even
+    public static boolean isEven(int a) {
+        if (a % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //is number odd
+    public static boolean isOdd(int a) {
+        return !isEven(a);
     }
 }
